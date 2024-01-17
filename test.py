576b4fb9464f353ed2f09aab8969f29152cc5775
cinemere <albina.k.p16kaa4@gmail.com>
2024-01-17 18:17:01 +0300

    add introduction to llvm-a2c task

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+# script for checking CompilerGym installation
 import gym
 import compiler_gym
+
 # env = compiler_gym.make("llvm-v0")
 env = gym.make("llvm-autophase-ic-v0")
+
 print(env)
+
 # help(env.step)
 # print(env.)
+
 env.reset()
+
 print(env)
